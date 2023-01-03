dc28810e291ff29d963df597937425e403eeae88
Neil Holloway <neil_holloway@hotmail.co.uk>
2023-01-03 10:49:50 +0000

    only download files that don't already exist, write binary, catch errors

--- scrape.py
+++ scrape.py
@@ -1,17 +1,25 @@
+import imp
 from bs4 import BeautifulSoup, SoupStrainer
+from bs4.element import Tag
 import urllib.request
 import requests
+import os
 
 with open("data_loc.html") as html:
     soup = BeautifulSoup(html)
 
 
-def process_a_elem_write_file(a_elem) -> None:
+def process_a_elem_write_file(a_elem: Tag, already_got_files: list[str]) -> None:
     GIMME_FILES = ["json", "csv", "zip", "xls", "docx", "txt", "kml", "xlsx"]
     href = a_elem["href"]
-    if href.rsplit(".")[-1] in GIMME_FILES:
-        resp = requests.get(href)
-        open(f"data/{get_file_name(href)}", "w").write(resp.content.decode("utf8"))
+    file_name = get_file_name(href)
+
+    if file_name not in already_got_files:
+        if href.rsplit(".")[-1] in GIMME_FILES:
+            resp = requests.get(href)
+            with open(f"data/{file_name}", "wb") as file:
+                file.write(resp.content)
+            print(f"downloaded: {href}")
 
 
 def get_file_name(href: str) -> str:
@@ -23,5 +31,9 @@ def get_file_name(href: str) -> str:
 
 
 if __name__ == "__main__":
+    already_got_files = os.listdir("data")
     for a_elem in soup.find_all("a"):
-        process_a_elem_write_file(a_elem)
+        try:
+            process_a_elem_write_file(a_elem, already_got_files)
+        except:
+            print(f"issue with: {a_elem}")
